caf670d0f2bff325929d7c71ebab35eb7fc00f78
Juan Manuel <185741215+ich-heisse-jvma@users.noreply.github.com>
2025-10-29 10:02:35 -0500

    Primer avance

---
+++ BackEnd/Infrastructure/Interfaces/IDepartamentoHospitalRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Infrastructure.Interfaces
+{
+    public interface IDepartamentoHospitalRepository
+    {
+        Task<IEnumerable<DepartamentoHospital>> GetAllAsync();
+        Task<DepartamentoHospital> GetByIdAsync(int id);
+        Task AddAsync(DepartamentoHospital departamento);
+        Task UpdateAsync(DepartamentoHospital departamento);
+        Task DeleteAsync(int id);
+    }
+}
